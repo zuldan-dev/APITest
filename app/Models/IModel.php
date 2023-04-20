e0501f4d906cae8bb2219cb614744225230f782e
Yevhenii Strashko <zuldan2@gmail.com>
2023-04-20 14:35:01 +0300

    Added dummy data and comments

--- app/Models/IModel.php
+++ app/Models/IModel.php
@@ -6,10 +6,22 @@ namespace App\Models;
  * @package App\Models
  */
 interface IModel {
-
+    /**
+     * @param array $data
+     * @return bool
+     */
     public function create( array $data = [] ): bool;
 
+    /**
+     * @param array $params
+     * @return array
+     */
     public function read( array $params = [] ): array;
 
+    /**
+     * @param array $params
+     * @param array $data
+     * @return bool
+     */
     public function update( array $params = [], array $data = [] ): bool;
 }
--- app/Utils/CustomMethods.php
+++ app/Utils/CustomMethods.php
@@ -7,13 +7,18 @@ namespace App\Utils;
  */
 class CustomMethods {
     /**
-     * Render 404 page
+     * Rendering 404 page
      */
     public static function get404(): void {
         http_response_code('404');
         echo "<h1>404</h1>";
     }
 
+    /**
+     * Rendering correct data
+     * @param array $rawData
+     * @param string $token
+     */
     public static function renderData( array $rawData, string $token ): void {
         $data = [
             'status' => 'Ok',
@@ -23,14 +28,27 @@ class CustomMethods {
         self::renderJson( $data );
     }
 
+    /**
+     * Rendering error message
+     * @param string $msg
+     */
     public static function renderError( string $msg ): void {
         self::renderMsg( $msg, 'Error' );
     }
 
+    /**
+     * Rendering info message
+     * @param string $msg
+     */
     public static function renderInfo( string $msg ): void {
         self::renderMsg( $msg );
     }
 
+    /**
+     * Rendering message
+     * @param string $msg
+     * @param string $status
+     */
     private static function renderMsg( string $msg, string $status = 'Ok' ): void {
         $data = [
             'status' => $status,
@@ -39,6 +57,10 @@ class CustomMethods {
         self::renderJson( $data );
     }
 
+    /**
+     * Rendering JSON
+     * @param array $data
+     */
     private static function renderJson( array $data = [] ):void {
         header('Content-Type: application/json');
         echo json_encode($data);
